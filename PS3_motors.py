dbc8548ab33e287c8128a93fb4a5706edfedcd5e
Gabriel Zepeda <gabriel.zepeda.a.93@gmail.com>
2016-04-22 04:02:17 +0000

    Added code.
    Added code.
    
    Signed-off-by: Gabriel Zepeda <gabriel.zepeda.a.93@gmail.com>

--- PS3_motors.py
+++ PS3_motors.py
@@ -24,6 +24,14 @@ MotorB0 = 23
 MotorB1 = 21
 MotorBE = 19
 
+Servo1 = 12
+Servo2 = 10
+Servo3 = 8
+
+value = 0
+max_val = 11.5
+min_val = 2 
+
 A0 = False
 A1 = False
 B0 = False
@@ -39,6 +47,14 @@ GPIO.setup(MotorB0,GPIO.OUT)
 GPIO.setup(MotorB1,GPIO.OUT)
 GPIO.setup(MotorBE,GPIO.OUT)
 
+GPIO.setup(Servo1, GPIO.OUT)
+GPIO.setup(Servo2, GPIO.OUT)
+GPIO.setup(Servo3, GPIO.OUT)
+
+S1 = GPIO.PWM(Servo1, 50)
+S2 = GPIO.PWM(Servo2, 50)
+S3 = GPIO.PWM(Servo3, 50)
+
 # Set all the Motors to 'off'
 GPIO.output(MotorA0, A0)
 GPIO.output(MotorA1, A1)
@@ -46,7 +62,9 @@ GPIO.output(MotorAE, False)
 GPIO.output(MotorBE, False)
 GPIO.output(MotorB0, B0)
 GPIO.output(MotorB1, B1)
-
+S1.start(2)
+S2.start(0)
+S3.start(0)
 
 # Only start the motors when the inputs go above the following threshold
 threshold = 0.60
@@ -55,6 +73,7 @@ threshold = 0.60
 LeftTrack = 0
 RightTrack = 0
 x=0
+y=0
 # Configure the motors to match the current settings.
 
 def setmotors():
@@ -99,14 +118,61 @@ try:
               if event.button == 11:
                   x = 0
                   print x 
+                  UpdateMotors = 1 
+          if event.type == pygame.JOYBUTTONDOWN:
+              print 'Etapa 1'
+              if event.button == 10:
+                  x = 10
+                  print x
+                  UpdateMotors = 1
+          elif event.type == pygame.JOYBUTTONUP:
+              print 'Etapa 1'
+              if event.button == 10:
+                  x = 0
+                  print x
                   UpdateMotors = 1
 
-            # Check if we need to update what the motors are doing
+          if event.type == pygame.JOYBUTTONDOWN:
+              print 'Etapa 1'
+              if event.button == 12:
+                  y = 12
+                  print y
+                  UpdateMotors = 1 
+          #elif event.type == pygame.JOYBUTTONDOWN:
+          #    print 'Etapa 1'
+          #    if event.button == 13:
+          #        x = 13
+          #        print x
+          #        UpdateMotors = 1
+          #elif event.type == pygame.JOYBUTTONDOWN:
+          #    print 'Etapa 1'
+          #    if event.button == 14:
+          #        x = 14
+          #        print x
+          #        UpdateMotors = 1
+          #elif event.type == pygame.JOYBUTTONDOWN:
+          #    print 'Etapa 1'
+          #    if event.button == 15:
+          #        x = 15
+          #        print x
+          #        UpdateMotors = 1
+
+
+# Check if we need to update what the motors are doing
           if UpdateMotors:
-              if (x==11):
-                  A0 = False
-                  A1 = True
-                  print 'holi'
+              #if (x==11):
+              #    A0 = False
+              #    A1 = True
+              #    print 'holi'
+              if (y==12):
+                  if (j.get_button(11) and value < max_val):
+                          value = value + 0.5 
+                          S1.ChangeDutyCycle(value)
+                          print 'To max val:' +  str(value) 
+                  elif (j.get_button(10) and value > min_val):
+                          value = value - 0.5
+                          S1.ChangeDutyCycle(value)
+                          print 'To min val' + str(value)
               # Check how to configure the left motor
               # Move forwards
               #if (RightTrack > threshold):
@@ -122,6 +188,13 @@ try:
                   A1 = False
 
               # And do the same for the right motor
+              #if (x == 12):
+              #    if (j.get_button(11) == 1 and value < max_val):
+              #        S1.ChangeDutyCycle(value)
+              #        value = value + 1
+              #    elif(j.get_button(10) == 1 and value > min_val):
+              #        S1.ChangeDutyCycle(value)
+              #        value = value - 1
               if (LeftTrack > threshold):
                   B0 = False
                   B1 = True
